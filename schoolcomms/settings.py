3286c3aa0d88a16d822818739a76cc34c56a5203
Saku_Up <uezt.isk@gmail.com>
2025-10-22 14:09:12 +0900

    本番環境CSS反映のコーディング内容コミット

--- schoolcomms/settings.py
+++ schoolcomms/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,7 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"  # 本番: collectstatic 置き場
 
 # Default primary key field type
